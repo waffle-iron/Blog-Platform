d2ed12f561bd0bf60ba9fec73dbba41f39eee4ab
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2017-02-15 23:46:54 +0000

    same method for both all and individual

--- src/Test.php
+++ src/Test.php
@@ -6,7 +6,12 @@ class Test extends App  {
 
     public function show($request, $response, $args)
     {
-        $posts = $this->getEntityManager()->find('Post', $args['id']);
+        if(empty($args['id'])) {
+        	$posts = $this->getEntityManager()->getRepository('Post')->findAll();
+        } else {
+        	$posts = $this->getEntityManager()->find('Post', $args['id']);
+        }
+        
         var_dump($posts);
     }
 }
