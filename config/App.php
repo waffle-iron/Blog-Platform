f89b66c559d3a6b138c7c033d7799625293cfd2b
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2017-02-15 22:30:12 +0000

    create entity manager and return

--- config/App.php
+++ config/App.php
@@ -2,6 +2,23 @@
 
 namespace Config;
 
+use \Doctrine\ORM\Tools\Setup;
+use \Doctrine\ORM\EntityManager;
+
 class App {
-    
+
+	public function getEntityManager()
+	{
+		$config  = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/../src"), true);
+
+		$conn = array(
+		  'driver' => 'pdo_mysql',
+		  'host'   => '127.0.0.1',
+		  'dbname' => 'doctrine',
+		  'user'   => 'root',
+		  'password' => 'root'
+		);
+
+		return EntityManager::create($conn, $config);
+	}
 }
