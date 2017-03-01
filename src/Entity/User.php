568fbb66a96c7b422f0c8a968cd69ec8509d60f2
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2017-03-01 20:51:24 +0000

    Delete Test classes, and create User Entity

---
+++ src/Entity/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * @Entity
+ **/
+
+class User {
+
+	/**
+	 * @Id
+	 * @Column(type="integer")
+	 * @GeneratedValue(strategy="AUTO")
+	 **/
+	protected $id;
+
+	/**
+	 * @Column(type="string", length=255, unique=true, nullable=false)
+	 **/
+	protected $email;
+
+	/**
+	 * @Column(type="string", length=255, nullable=false)
+	 **/
+	protected $password;
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
+
+	public function getPassword()
+	{
+		return $this->getPassword();
+	}
+
+	public function setPassword($password)
+	{
+		$this->password = password_hash($password, PASSWORD_BCRYPT);
+	}
+}
